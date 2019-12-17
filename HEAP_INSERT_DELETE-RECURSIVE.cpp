7b34e04677f02c5783c629e3566871f93b51886b
PiyushKhanna30 <khanna.piyush30@gmail.com>
2019-12-17 16:50:35 +0530

    heap recursive

---
+++ HEAP_INSERT_DELETE-RECURSIVE.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+using namespace std;
+
+class heap
+{
+    int n,h[100];
+public:
+    heap()
+    {
+        cout<<"\nEnter number of elements in heap - ";
+        cin>>n;
+        cout<<"\nEnter numbers - ";
+        for(int i=0;i<=n-1;i++)
+        {
+            cin>>h[i];
+        }
+    }
+    void swap(int *a,int *b)
+    {
+        int t=*a;
+        *a=*b;
+        *b=t;
+    }
+    void insertVerify(int value_index)
+    {
+        int parent_index=(value_index-1)/2;
+        if (parent_index>=0)
+        {
+            if (h[parent_index]<h[value_index])
+            {
+                swap(&h[value_index],&h[parent_index]);
+                value_index=parent_index;
+                insertVerify(value_index);
+            }
+        }
+    }
+    void insert(int value)
+    {
+        n++;
+        h[n-1]=value;
+        insertVerify(n-1);
+    }
+    void deleteVerify(int index)
+    {
+        int left_ch,right_ch,par;
+        par=index;
+        left_ch=(2*par)+1;
+        right_ch=(2*par)+2;
+        if (h[left_ch]>h[par] && left_ch<(n-1) )
+        {
+            par=left_ch;
+        }
+        if (h[right_ch]>h[par] && right_ch<(n-1))
+        {
+            par=right_ch;
+        }
+        if (index!=par)
+        {
+            swap(&h[index],&h[par]);
+            index=par;
+            deleteVerify(index);
+        }
+    }
+    void delet()
+    {
+        h[0]=h[n-1];
+        n--;
+        deleteVerify(0);
+    }
+    void show()
+    {
+        cout<<"\nResult is - ";
+        for(int i=0;i<=n-1;i++)
+        {
+            cout<<h[i]<<" ";
+        }
+    }
+};
+int main()
+{
+    heap obj;
+    obj.insert(15);
+    obj.show();
+    obj.delet();
+    obj.show();
+}
