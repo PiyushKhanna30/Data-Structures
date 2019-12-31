76feb65372f00500fc1e470b5a3d223a53667d6e
PiyushKhanna30 <khanna.piyush30@gmail.com>
2019-12-31 22:33:39 +0530

    added bst find loc fxn

---
+++ BST_FIND_LOC.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+    node(int data)
+    {
+        this->data=data;
+        left=NULL;
+        right=NULL;
+    }
+};
+class Find
+{
+    node *par,*loc,*ptr,*save;
+public:
+    node *fxn(node *root,int item)
+    {
+        if (root==NULL)
+        {
+            loc=NULL;
+            par=NULL;
+            cout<<"\n1\n";
+            return(loc);
+        }
+        else if(root->data==item)
+        {
+            loc=root;
+            par=NULL;
+            cout<<"2\n";
+            return(loc);
+        }
+        else
+        {
+            if ((root->data)>item)
+            {
+                par=root;
+                root=root->left;
+            }
+            else
+            {
+                par=root;
+                root=root->right;
+            }
+            while(root!=NULL)
+            {
+                if (item==(root->data))
+                {
+                    loc=root;
+                    cout<<"\n3\n";
+                    return(loc);
+                }
+                else if(item<(root->data))
+                {
+                    par=root;
+                    root=root->left;
+                }
+                else
+                {
+                    par=root;
+                    root=root->right;
+                }
+            }
+        }
+        loc=NULL;
+        cout<<"\n4\n";
+        return(loc);
+    }
+};
+int main()
+{
+    node *temp=new node(38);
+    temp->left=new node(14);
+    temp->left->left=new node(8);
+    temp->left->right=new node(23);
+    temp->left->right->left=new node(10);
+    temp->right=new node(58);
+    temp->right->left=new node(45);
+    temp->right->right=new node(82);
+    temp->right->right->left=new node(70);
+    Find f;
+    cout<<f.fxn(temp,14);
+}
